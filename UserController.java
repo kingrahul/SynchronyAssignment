e40ca402c0270eb9c3e4a72829043edb142f3b6d
Rahul Kumar <rahulkumar.bhumca11@gmail.com>
2023-03-28 12:02:20 +0530

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,21 @@
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User savedUser = userService.registerUser(user);
+        return ResponseEntity.created(URI.create("/api/users/" + savedUser.getId())).body(savedUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.getUser(id);
+        return ResponseEntity.ok(user);
+    }
+
+    // other endpoints for updating user information and associating images with the user
+}
